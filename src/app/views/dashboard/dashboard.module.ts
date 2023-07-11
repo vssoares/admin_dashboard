d2f8214003f0d85fbc8b0bdfa19d3d8cdc24490d
vssoares23 <vsss23@hotmail.com>
2023-07-11 19:43:50 -0300

    525

--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard.routing';
+import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
+import { SidebarComponent } from 'src/app/shared/components/sidebar/sidebar.component';
 
 @NgModule({
-   imports: [CommonModule, DashboardRoutingModule],
+   imports: [
+      CommonModule,
+      DashboardRoutingModule,
+      NavbarComponent,
+      SidebarComponent,
+   ],
    declarations: [DashboardComponent],
 })
 export class DashboardModule {}
